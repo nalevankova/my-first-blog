a2ca1f72bb7eb42551a75e05931cec47f0b65e37
Jana <janaamara@centrum.sk>
2024-09-21 15:25:01 +0200

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def ahoj(meno):
+    print('Ahoj ' + meno + '!')
+
+dievcata = ['Katka', 'Kika', 'Zuza', 'Ola', 'Ty']
+for meno in dievcata:
+    ahoj(meno)
+    print('Dalsie dievca')
+
+for i in range(1, 6):
+    print(i)
+    
